41dbd0bc8ea9dd2957814536f4cd5cf14a4ca1f2
leduyvuong <leduyvuong99@gmail.com>
2021-08-06 10:15:34 +0700

    add Word List Model and Lesson Model

---
+++ app/models/wordlist.rb
@@ -0,0 +1,4 @@
+class Wordlist < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+end
---
+++ db/migrate/20210806030901_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :lessons do |t|
+      t.string :name_lesson
+      t.boolean :status
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
